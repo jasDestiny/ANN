b4222cb7962afe1b5ee967a9c6417cc797f5b042
John Arthur Samuel D <49168571+jasDestiny@users.noreply.github.com>
2020-07-25 14:31:01 +0530

    Create solution.py

---
+++ Perceptron Network/solution.py
@@ -0,0 +1,46 @@
+def activationFunction(value):
+    if value>=1:
+        return 1
+    elif value<=-1:
+        return -1
+    else:
+        return 0
+    
+def perceptron(inputs, learning_rate, target):
+    weights=[0 for i in inputs[0]]
+    outputs=[]
+    delW=[]
+    y=[float("inf") for x in target]
+    epoch=0
+    while target!=y:
+        epoch+=1
+        print("Epoch #",epoch)
+        for i in range(len(inputs)):
+            yin=0
+
+            for j in range(len(inputs[i])):
+                yin+=inputs[i][j]*weights[j]
+            y[i]=activationFunction(yin)
+            outputs.append(y[i])
+            thisDelW=[]
+
+            print("yin :",yin,"y :",y[i])
+            for j in range(len(inputs[0])):
+                thisDelW.append(learning_rate*inputs[i][j]*target[i])
+            delW.append(thisDelW)
+
+
+            if y[i]!=target[i]:
+                for i in range(len(weights)):
+                    weights[i]+=thisDelW[i]
+    return weights
+                
+
+perceptron([[1,1,1],[1,-1,1],[-1,1,1],[-1,-1,1]],1,[-1,1,-1,-1])
+
+        
+        
+        
+        
+    
+    
